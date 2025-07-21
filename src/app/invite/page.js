f279349f22a216cba814ec2ada3cb3695f3080bf
DevCrypto187 <crypto_dev@marzmain.io>
2025-07-21 19:16:13 +0900

    minddgame add useeffect

--- src/app/invite/page.js
+++ src/app/invite/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AnimatePresence, motion } from 'framer-motion';
 import Alert from '@mui/material/Alert';
 
